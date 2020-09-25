036f97a206ae928f84976c3887909fabf2de6a3c
Brett T. Hannigan <brett.hannigan@inscripta.com>
2020-09-25 10:56:21 -0600

    Simple package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_namespace_packages
+
+with open("requirements_dev.txt") as reqs_file:
+    requirements_dev = reqs_file.readlines()
+
+install_requires = requirements_dev
+
+setup(
+    name="urban-octo-palm-tree",
+    version="1.0.0",
+    author="Brett T. Hannigan",
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython"
+    ],
+    description="Dummy package",
+    long_description="Dummy Package for testing CI",
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={},
+    packages=find_namespace_packages(include=["urban_octo_palm_tree.*"]),
+    test_suite="pytest",
+    tests_require=requirements_dev,
+)
