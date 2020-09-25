a33f85625b6f421ed9e4296857fd6c8efaa7aec2
Brett T. Hannigan <brett.hannigan@inscripta.com>
2020-09-25 10:09:14 -0600

    Some simple tests

---
+++ tests/test_simple.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.parametrize("input_value", [1, True, False, "hello"])
+def test_success(input_value):
+    assert input_value == input_value
+
+@pytest.mark.parametrize("input_value", [1, True, False, "hello"])
+def test_failure(input_value):
+    assert input_value != input_value
