09550a0303ea87262b3b95409e86aff3db88094e
Brett T. Hannigan <brett.hannigan@inscripta.com>
2020-10-01 16:32:56 -0600

    Add MANIFEST.in Correct tox.ini

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,8 +1,10 @@
 import pytest
 
+
 @pytest.mark.parametrize("input_value", [1, True, False, "hello"])
 def test_success(input_value):
     assert input_value == input_value
+
 
 @pytest.mark.parametrize("input_value", [1, True, False, "hello"])
 def test_failure(input_value):
